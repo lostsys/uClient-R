286fec1b4cfb73b163006b84b225addc9ede42ce
VengeUrban <71067316+VengeUrban@users.noreply.github.com>
2020-12-15 22:13:54 +0800

    Fixed adblocker

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded",() => {
     console.log = function(e){
-        if (e == "show main ui")  hideAds();
         if (e == "intermission") adplayer = undefined;
+    }
+    window.requestAnimationFrame(check);
+    function check(){
+        let code = ["monetization","idTips"];
+        code.forEach((e) => {
+            if (typeof document.getElementById(e) == "undefined") return;
+            if (document.getElementById(e).style.display != "none") document.getElementById(e).style.display = "none";
+        })
+        window.requestAnimationFrame(check);
     }
 })
 
